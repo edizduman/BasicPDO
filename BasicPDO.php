44dee3b7d50c3e65f4562b1b85b8bb6d785829a4
Ediz Duman <eduman@otoanaliz.net>
2015-04-10 16:38:00 +0300

    SQL ler genişletildi

--- BasicPDO.php
+++ BasicPDO.php
@@ -1,134 +1,205 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ediz
- * Date: 29.3.2015
- * Time: 23:23
- *
- * Singleton PDO Sınıfı
- */
+	/**
+	 * Created by PhpStorm.
+	 * User: ediz
+	 * Date: 29.3.2015
+	 * Time: 23:23
+	 *
+	 * Singleton PDO Sınıfı
+	 * https://github.com/edizduman/BasicPDO
+	 */
 
-namespace DB;
+	namespace DB;
 
-use PDO;
-
-
-class BasicPDO
-{
-
-    /*** mysql hostname ***/
-    private static $hostname = 'localhost'; // Put your host name here
-    /*** mysql username ***/
-    private static $username = 'root'; // Put your MySQL User name here
-    /*** mysql password ***/
-    private static $password = 'root'; // Put Your MySQL Password here
-    /*** mysql password ***/
-    private static $dbName = 'kys'; // Put Your MySQL Database name here
+	use PDO;
 
 
-    /**
-     * SQL'in tutulduğu değişken
-     * @var
-     */
-    private static $sql;
+	class BasicPDO
+	{
 
-    private static $tableName;
-
-    public static $db = NULL;
-
-    /**
-     * Sınıf içerisinde tanımlı olamayan ve
-     * çağrılan tüm fonksiyonları PDO içersinden çağırır
-     */
-    public static function __callStatic($func, $args)
-    {
-        return call_user_func_array(
-            array(self::getConnettion(), $func),
-            $args
-        );
-    }
+		/*** mysql hostname ***/
+		private static $hostname = 'localhost'; // Put your host name here
+		/*** mysql username ***/
+		private static $username = 'root'; // Put your MySQL User name here
+		/*** mysql password ***/
+		private static $password = ''; // Put Your MySQL Password here
+		/*** mysql password ***/
+		private static $dbName = 'kys'; // Put Your MySQL Database name here
 
 
-    /**
-     * Bir bağlantı mevcut ise o bağlantıyı döndür.
-     * Bağlantı Yoksa init'i tekrar tetikleyerek bağlantıyı başlatır
-     * Sadece run() fonksiyonunda kullanıyoruz.
-     *
-     */
-    public static function getConnection()
-    {
-        return
-            self::$db == null ?
-                self::init() :
-                self::$db;
-    }
+		/**
+		 * SQL'in tutulduğu değişken
+		 * @var
+		 */
+		private static $sql;
 
-    /**
-     * BasicPDO kurucu metodu
-     *
-     * @param $host
-     * @param $dbname
-     * @param $username
-     * @param $password
-     * @param string $charset
-     */
-    private static function init()
-    {
+		/**
+		 * WHERE'in tutulduğu değişken
+		 * @var
+		 */
+		private static $whereValue = array ();
 
-        try {
-            $dsn = 'mysql:host=' . static::$hostname . ';dbname=' . static::$dbName;
-            self::$db = new PDO($dsn, static::$username, static::$password);
-            self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            self::$db->exec('SET CHARACTER SET utf8');
-            return self::$db;
-
-        } catch (PDOException $e) {
-            echo __LINE__ . $e->getMessage() . "Hata";
-        }
-
-    }
-
-    public static function select($_tableName)
-    {
-        self::$sql = "SELECT * FROM " . $_tableName;
-
-    }
+		/**
+		 * WHERE değerlerinin tutulduğu değişken
+		 * @var
+		 */
+		private static $where = false;
 
 
-    public static function run($single = false)
-    {
-        $query = self::getConnection()->query(self::$sql);
-        if ($single)
-            return $query->fetch();
-        else
-            return $query->fetchAll();
-    }
+		/**
+		 * Insert veya Update için set edilecek column
+		 * @var
+		 */
+
+		private static $columns = false;
+
+		/**
+		 * Insert veya Update için prepare edilecek column değerleri
+		 * @var
+		 */
+		private static $columnsValue = array ();
 
 
-    /*public static function errormessage(PDOException $ex) {
-        $kodeerror  = sprintf("%0.0f", strstr($ex->getMessage(), '1'));
-        $regex      = preg_replace("#[^\w()/.%\-&/']#", " ", $ex->getMessage());
-        $kesalahan  = substr($regex, strpos($regex, '1') + strlen($ex->getCode()));
-        switch ($ex->getCode()) {
-            case '1045':
-            case '1049':
-                echo "<div class='container'><h1>Terjadi Kesalahan Database</h1>
-                      <p>Kode Error : " . $kodeerror . "</p>
-                      <p>Kesalahan : " . $kesalahan . "</p>
-                      <p>Nama File : " . $ex->getFile() . "</p>
-                      <p>Baris : " . $ex->getLine() . "</p></div>";
-                break;
-            case '42S02':
-            case '42S22':
-            case '42000':
-            case '23000':
-                echo "<div class='container'><h1>Terjadi Kesalahan Database</h1>
-                      <p>Kode Error : " . $kodeerror . "</p>
-                      <p>Kesalahan : " . $kesalahan . "</p>
-                      <p>Nama File : " . $ex->getTrace()[1]['file'] . "</p>
-                      <p>Baris : " . $ex->getTrace()[1]['line'] . "</p></div>";
-                break;
-        }
-    }*/
+		/**
+		 * LIMIT'in tutulduğu değişken
+		 * @var
+		 */
+		private static $limit = false;
 
-}
+		private static $tableName;
+
+		public static $db = NULL;
+
+		/**
+		 * Sınıf içerisinde tanımlı olamayan ve
+		 * çağrılan tüm fonksiyonları PDO içersinden çağırır
+		 */
+		public static function __callStatic ($func,$args)
+		{
+			return call_user_func_array (array (self::getConnettion (),$func),$args);
+		}
+
+
+		/**
+		 * Bir bağlantı mevcut ise o bağlantıyı döndür.
+		 * Bağlantı Yoksa init'i tekrar tetikleyerek bağlantıyı başlatır
+		 * Sadece run() fonksiyonunda kullanıyoruz.
+		 *
+		 */
+		public static function getConnection ()
+		{
+			return self::$db == null ? self::init () : self::$db;
+		}
+
+		/**
+		 * BasicPDO kurucu metodu
+		 *
+		 * @param $host
+		 * @param $dbname
+		 * @param $username
+		 * @param $password
+		 * @param string $charset
+		 */
+
+		private static function init ()
+		{
+			try {
+				$dsn      = 'mysql:host=' . static::$hostname . ';dbname=' . static::$dbName;
+				self::$db = new PDO($dsn,static::$username,static::$password);
+				self::$db->setAttribute (PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+				self::$db->exec ('SET CHARACTER SET utf8');
+				return self::$db;
+			}
+			catch (PDOException $e) {
+				echo __LINE__ . $e->getMessage () . "Hata";
+			}
+
+		}
+
+		public static function select ($_tableName)
+		{
+			self::$sql = "SELECT * FROM " . $_tableName;
+		}
+
+		public static function insert ($_tableName)
+		{
+			self::$sql = "INSERT INTO  " . $_tableName;
+		}
+
+		public static function update ($_tableName)
+		{
+			self::$sql = "UPDATE " . $_tableName;
+		}
+
+		public static function delete ($_tableName)
+		{
+			self::$sql = "DELETE FROM " . $_tableName;
+		}
+
+		public static function setColumns ($_columns,$_param = ',')
+		{
+			self::$columns      = " SET ";
+			self::$columnsValue = array ();
+			foreach ($_columns as $key => $val) {
+				self::$columns .= ' ' . $key . ' = ? ' . $_param;
+				self::$columnsValue[] = $val;
+			}
+			self::$columns = substr (self::$columns,0,-strlen ($_param));
+		}
+
+
+		public static function setWhere ($_where,$_param = 'and')
+		{
+			self::$where      = " WHERE ";
+			self::$whereValue = array ();
+			foreach ($_where as $key => $val) {
+				self::$where .= ' ' . $key . ' = ? ' . $_param;
+				self::$whereValue[] = $val;
+			}
+			self::$where = substr (self::$where,0,-strlen ($_param));
+		}
+
+		public static function setLimit ($limit)
+		{
+			self::$limit = "LIMIT " . $limit;
+		}
+
+		public static function setFrom ($from)
+		{
+			self::$sql = str_replace ('*',$from,self::$sql);
+		}
+
+
+		public static function run ($single = false)
+		{
+			$value = array ();
+			if (self::$columns != false) {
+				self::$sql .= self::$columns;
+				$value = self::$columnsValue;
+			}
+
+			if (self::$where != false) {
+				self::$sql .= self::$where;
+				$value = array_merge ($value,self::$whereValue);
+			}
+
+			echo self::$sql;
+
+			$query = self::getConnection ()->prepare (self::$sql);
+			$query->execute ($value);
+			if (self::$columns == false) {
+				self::$where = false;
+				if ($single) {
+					return $query->fetch ();
+				} else {
+					return $query->fetchAll ();
+				}
+			} else {
+				self::$columns = false;
+				self::$where   = false;
+				return $query;
+			}
+		}
+
+
+	}
--- test.php
+++ test.php
@@ -1,16 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ediz
- * Date: 29.3.2015
- * Time: 23:50
- */
+	/**
+	 * Created by PhpStorm.
+	 * User: ediz
+	 * Date: 29.3.2015
+	 * Time: 23:50
+	 */
 
-include "BasicPDO.php";
+	include "BasicPDO.php";
 
-use DB\BasicPDO as DB;
+	use DB\BasicPDO as DB;
 
-DB::select("users");
-$result = DB::run();
-var_dump($result);
+
+
+/*	DB::insert("users");
+	DB::setColumns(array('username'=>'aa','password'=>'aa'));
+	DB::run();
+
+	DB::select ("users");
+	DB::setFrom ('username,password');
+	DB::setWhere(array('username'=>'c' ,'password'=>'c'));
+	DB::run ();
+
+	DB::update("users");
+	DB::setColumns(array("username"=>"ediz"));
+	DB::setWhere(array("id"=>"1"));
+	DB::run();*/
+
+	DB::delete("users");
+	DB::setWhere(array('id'=>3));
+	DB::run();
+
+
 
