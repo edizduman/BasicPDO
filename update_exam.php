99553ba3e9d3438abd6c265ab221828292f28fa1
Ediz Duman <ben@edizduman.com>
2015-06-26 16:58:38 +0300

    Update kullanım örneği

--- update_exam.php
+++ update_exam.php
@@ -5,3 +5,14 @@
  * Date: 26.6.2015
  * Time: 16:53
  */
+
+include "Config.php";
+include "BasicPDO.php";
+
+\Config\Config::readConfig();
+use DB\BasicPDO as DB;
+
+DB::update("users");
+DB::setColumns(array("name"=>"ediz","surname"=>"duman"));
+DB::setWhere(array("id"=>"1"));
+DB::run();
